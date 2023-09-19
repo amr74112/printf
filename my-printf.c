a97b1540f463177e53fa6ed7b0ea9b35513fbd39
Eslam0sama <eslam.osama.abdullah@gmail.com>
2023-09-18 18:54:40 -0700

    printf function defined here

---
+++ my-printf.c
@@ -0,0 +1,50 @@
+#include <stdarg.h>
+
+/**
+ * _printf - A function like original _printf function
+ * @format: is a string that contains format specifiers
+ * @...: arguments to be formated and printed
+ * @format: is a string that contains format specifiers
+ * Return: The return value
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list args;
+
+	va_start(args, format);
+	for (int i = 0; format[i] != '\0'; i++)
+	{
+		char CHARACTER;
+		char *STRING;
+		int NUM;
+
+		if (format[i] == '%' && (format[i + 1] == 'd' || format[i + 1] == 'i'))
+		{
+			NUM = va_arg(args, int);
+			PUT_INT(NUM);
+			i++;
+		}
+		else if (format[i] == '%' && format[i + 1] == 'c')
+		{
+			CHARACTER = (char)va_arg(args, int);
+			PUTCHAR(CHARACTER);
+			i++;
+		}
+		else if (format[i] == '%' && format[i + 1] == 's')
+		{
+			STRING = va_arg(args, char*);
+			PUTS(STRING);
+			i++;
+		}
+		else if (format[i] == '%' && format[i + 1] == '%')
+		{
+			PUTCHAR('%');
+			i++;
+		}
+		else
+			 PUTCHAR(format[i]);
+	}
+		va_end(args);
+		return (0);
+}
