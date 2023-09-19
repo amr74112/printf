ef2899a3eb0afc5fb1789836f42dc69c8f99f602
Eslam0sama <eslam.osama.abdullah@gmail.com>
2023-09-18 19:48:10 -0700

    main headers

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+void PUTCHAR(char x);
+void PUTS(const char *x);
+void PUT_INT(int x);
+int _printf(const char* format, ...);
+
+
+
+#endif // MAIN_H_INCLUDED
+
