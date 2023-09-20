2f27922f67fea36a9f3e44d6e7c8a3f5c40c2779
Eslam0sama <eslam.osama.abdullah@gmail.com>
2023-09-19 19:18:37 -0700

    edit done on main.h

--- main.h
+++ main.h
@@ -2,12 +2,13 @@
 #define MAIN_H_INCLUDED
 #include <stdarg.h>
 #include <stdio.h>
+#include <stdio.h>
 #include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-void PUTCHAR(char x);
-void PUTS(const char *x);
+int PUTCHAR(char x);
+int PUTS(const char *x);
 void PUT_INT(int x);
 int _printf(const char *format, ...);
 
