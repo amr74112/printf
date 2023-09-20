c3b07adabb7564b20cc6e12617715683225f5e11
Eslam0sama <eslam.osama.abdullah@gmail.com>
2023-09-19 18:30:37 -0700

    edit

--- my-printf.c
+++ my-printf.c
@@ -1,49 +1,60 @@
 #include "main.h"
+#include <stdarg.h>
+#include <stddef.h>
+
 /**
- * _printf - A function like original _printf function
- * @format: is a string that contains format specifiers
+ * _printf - A function like the original printf function
+ * @format: a string that contains format specifiers
  * Return: The return value
  */
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int i;
+    va_list args;
+    int i, count = 0;
 
-	va_start(args, format);
-	if (format == NULL)
-		return (-1);
-	for (i = 0; format && format[i] != '\0'; i++)
-		{
-		char CHARACTER, *STRING;
+    va_start(args, format);
+    if (format == NULL)
+        return (-1);
 
-		if (format[i] == '%' && format[i + 1] == 'c')
-		{
-			CHARACTER = (char)va_arg(args, int);
-			if (STRING == NULL)
-				return (-1);
-			PUTCHAR(CHARACTER);
-			i++;
-		}
-		else if (format[i] == '%' && format[i + 1] == 's')
-		{
-			STRING = va_arg(args, char*);
-			if (STRING == NULL)
-				return (-1);
-			PUTS(STRING);
-			i++;
-		}
-		else if (format[i] == '%' && format[i + 1] == '%')
-		{
-			PUTCHAR('%');
-			i++;
-		}
-		else
-		{
-			if (format == '\0')
-				break;
-			PUTCHAR(format[i]);
-		}
+    for (i = 0; format[i] != '\0'; i++)
+    {
+        char CHARACTER;
+        char *STRING;
 
-	}
-	return (i);
+        if (format[i] == '%')
+        {
+            i++;
+            switch (format[i])
+            {
+                case 'c':
+                    CHARACTER = (char)va_arg(args, int);
+                    PUTCHAR(CHARACTER);
+                    count++;
+                    break;
+                case 's':
+                    STRING = va_arg(args, char*);
+                    if (STRING == NULL)
+                        return (-1);
+                    PUTS(STRING);
+                    count++;
+                    break;
+                case '%':
+                    PUTCHAR('%');
+                    count++;
+                    break;
+                default:
+                    PUTCHAR('%');
+                    PUTCHAR(format[i]);
+                    count += 2;
+            }
+        }
+        else
+        {
+            PUTCHAR(format[i]);
+            count++;
+        }
+    }
+
+    va_end(args);
+    return (count);
 }
